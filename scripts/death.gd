1ae00db0599e6ca98ed1940dcfd4357732a391bb
Aidan Wong <anominosgamer@gmail.com>
2026-07-25 14:13:12 +0100

    animated (kinda) death screen

--- scripts/death.gd
+++ scripts/death.gd
@@ -1,13 +1,21 @@
 extends CanvasLayer
 
+var cur_scale = 0.0
 
 func _ready():
 	visible = false
 
-
-func _show():
+func _on_death():
+	cur_scale = 0
 	visible = true
 
+func _process(delta):
+	if cur_scale < 3:
+		var count_scale = min(cur_scale, 1)
+		var down_scale = min(max(0, cur_scale-1.5), 1)
+		$ColorRect/Count.scale = Vector2(count_scale, count_scale)
+		$ColorRect/Down.scale = Vector2(down_scale, down_scale)
+		cur_scale += 0.025
 
 func _on_restart():
 	visible = false
