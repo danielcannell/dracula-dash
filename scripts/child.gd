5ff8dee40e53ac0d4d8f82efac6a83f65c3d1d0a
Daniel Cannell <>
2026-07-25 15:43:01 +0100

    Fix child collision

--- scripts/child.gd
+++ scripts/child.gd
@@ -1,14 +1,9 @@
 extends StaticBody2D
 
-var scroll_speed := 300.0
-
 func _ready():
 	add_to_group("obstacles")
 
-func set_scroll_speed(new_speed: float):
-	scroll_speed = new_speed
-
 func _process(delta):
-	position.y += scroll_speed * delta
+	position.y += delta * Globals.cur_forward_speed
 	if position.y > 1000:
 		queue_free()
