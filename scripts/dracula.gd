1bf6a451b2ae4c890046e059a0a4ee4a9ce37b80
Robert McGregor <rmcgregor1990@gmail.com>
2026-07-25 16:08:35 +0100

    blood tire trails

--- scripts/dracula.gd
+++ scripts/dracula.gd
@@ -1,5 +1,21 @@
 extends CharacterBody2D
 
+@onready var wheel_fl: Marker2D = $WheelFL
+@onready var wheel_fr: Marker2D = $WheelFR
+@onready var wheel_bl: Marker2D = $WheelBL
+@onready var wheel_br: Marker2D = $WheelBR
+
+# trail
+var trail_scene: PackedScene = preload("res://scenes/trail.tscn")
+@onready var trail_container: Node2D = get_tree().current_scene.get_node("TrailContainer")
+var active_trail_bl: Line2D
+var active_trail_br: Line2D
+const MAX_TRAIL_POINTS := 80
+
+# wheels and thier trails - filled in on ready
+var wheels = []
+var wheel_trails = []
+
 signal dead
 signal blood(health: float)
 
@@ -22,9 +38,16 @@ var base_pos: Vector2
 
 @onready var spawner = get_node("/root/Main/ObstacleSpawner")
 
-func _ready():
+func _ready() -> void:
 	add_to_group("player")
 	base_pos = position
+	var items = [wheel_fl, wheel_fr, wheel_bl, wheel_br]
+	for item in items:
+		wheels.append(item)
+		var scene = trail_scene.instantiate()
+		wheel_trails.append(scene)
+		trail_container.add_child(scene)
+
 
 func _process(delta: float) -> void:
 	# blood_level -= delta * BLOOD_DRAIN_RATE
@@ -32,45 +55,56 @@ func _process(delta: float) -> void:
 		blood_level = 0
 		emit_signal("dead")
 	emit_signal("blood", blood_level)
-	
-	
+
+
 func _physics_process(delta: float) -> void:
 	match state:
 		State.NORMAL:
 			_normal_movement(delta)
+			_process_wheel_trail()
 		State.STUNNED:
 			_stunned_movement(delta)
 
+	for i in range(len(wheels)):
+		var pos = wheels[i].global_position - trail_container.position
+		_extend_trail(wheel_trails[i], pos)
+		
+func _process_wheel_trail():
+	for i in range(len(wheels)):
+		var pos = wheels[i].global_position - trail_container.position
+		_extend_trail(wheel_trails[i], pos)
+
 func _normal_movement(delta: float) -> void:
-	# Get input in range [-1, 1]
+	# Get input in rang e [-1, 1]
 	var turn = Input.get_axis("turn_left", "turn_right")
 	var dodge = Input.get_axis("dodge_up", "dodge_down")
-	
+
 	const kf = 0.9
 	var dodge_return_force = (position - base_pos).y * kf
 	if abs(dodge_return_force) < 0.001:
 		dodge_return_force = 0.0
 	var dodge_force = dodge * 100
-	
+
 	# Exponential smoothing of direction angle
 	var alpha = 1 - exp(-delta / TURN_TIME_SCALE_S)
 	angle += alpha * (turn - angle)
-	
+
 	# Rotate the whole scene for the correct facing
 	rotation = angle
-	
+
 	# Physics
 	var vspeed = (dodge_force - dodge_return_force) * delta * 400
 	velocity = SPEED * Vector2(sin(angle), 0) + Vector2(0, vspeed)
 	move_and_slide()
-	
+
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		var collider = collision.get_collider()
 		if collider.is_in_group("obstacles"):
 			_on_hit(collision)
 			break
-			
+
+
 func _stunned_movement(delta: float) -> void:
 	velocity = bounce_velocity
 	move_and_slide()
@@ -91,3 +125,10 @@ func _on_hit(collision: KinematicCollision2D) -> void:
 func _freeze_scroll(frozen: bool) -> void:
 	spawner.set_paused(frozen)
 	Globals.cur_forward_speed = 0.0 if frozen else 300.0
+
+
+func _extend_trail(line: Line2D, pos: Vector2) -> void:
+	if line.get_point_count() == 0 or pos.distance_to(line.points[-1]) > 4.0:
+		line.add_point(pos)
+		if line.get_point_count() > MAX_TRAIL_POINTS:
+			line.remove_point(0)
