45e6b65e847bfcaba6acdf919bdd87fb1651fd75
Aidan Wong <anominosgamer@gmail.com>
2026-07-25 15:36:15 +0100

    blood explosion

--- scenes/main.gd
+++ scenes/main.gd
@@ -30,3 +30,5 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_X:
 			$Dracula.dead.emit()
+		if event.pressed and event.keycode == KEY_B:
+			$BloodExplode.restart()
