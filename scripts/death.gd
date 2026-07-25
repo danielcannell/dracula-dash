1bfe3939863da0487701a61d4241d5f37f70c2c5
Aidan Wong <anominosgamer@gmail.com>
2026-07-25 17:05:14 +0100

    fix scaling text to be from centre

--- scripts/death.gd
+++ scripts/death.gd
@@ -2,21 +2,22 @@ extends CanvasLayer
 
 var cur_scale = 0.0
 
+
 func _ready():
 	visible = false
 
 func _on_death():
 	if not visible:
 		cur_scale = 0
-		$ColorRect/Restart.grab_focus()
+		$ColorRect/VBoxContainer/Restart.grab_focus()
 		visible = true
 
 func _process(delta):
 	if cur_scale < 3:
 		var count_scale = min(cur_scale, 1)
 		var down_scale = min(max(0, cur_scale-1.5), 1)
-		$ColorRect/Count.scale = Vector2(count_scale, count_scale)
-		$ColorRect/Down.scale = Vector2(down_scale, down_scale)
+		$ColorRect/VBoxContainer/Count/Count.scale = Vector2(count_scale, count_scale)
+		$ColorRect/VBoxContainer/Down/Down.scale = Vector2(down_scale, down_scale)
 		cur_scale += 1.0 * delta
 
 func _on_restart():
