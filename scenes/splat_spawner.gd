aac05aa4d6a8e5e9c435945e34a2928890304432
Jamie Wood <hypernerd1995@gmail.com>
2026-07-25 16:27:08 +0100

    Replace splat placeholder

--- scenes/splat_spawner.gd
+++ scenes/splat_spawner.gd
@@ -6,7 +6,7 @@ func make_splat(pos: Vector2) -> void:
 	var splat = splat_scene.instantiate()
 	add_child(splat)
 
-	var splat_scale = randf_range(0.8, 2.5)
+	var splat_scale = randf_range(1.5, 3.5)
 	splat.global_position = pos
 	splat.scale = Vector2(1, 1) * splat_scale
-	splat.rotation = randf_range(-0.3, 0.3)
+	splat.rotation = randf_range(-0.5, 0.5)
