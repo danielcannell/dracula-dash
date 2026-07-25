03bd319aaebec2074fcb5391b3cc5ce7a7a66d5b
Daniel Cannell <>
2026-07-25 11:29:44 +0100

    And turn right/left to input map

--- scripts/dracula.gd
+++ scripts/dracula.gd
@@ -5,5 +5,10 @@ var acceleration = Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("turn_left"):
+		print("x")
+	if Input.is_action_pressed("turn_right"):
+		print("y")
+
 	velocity += acceleration * delta
 	move_and_slide()
