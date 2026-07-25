12c2a0cc311730e22605d407ff7e732e320eef37
Daniel Cannell <>
2026-07-25 12:08:07 +0100

    Collision with edges of road

--- scripts/dracula.gd
+++ scripts/dracula.gd
@@ -1,14 +1,24 @@
 extends CharacterBody2D
 
 
+const TURN_TIME_SCALE_S = 0.1
+const SPEED = 1000
+
+
 var angle: float = 0
 
 
 func _physics_process(delta: float) -> void:
+	# Get input in range [-1, 1]
 	var turn = Input.get_axis("turn_left", "turn_right")
-	angle += 0.1 * (turn - angle)
+	
+	# Exponential smoothing of direction angle
+	var alpha = 1 - exp(-delta / TURN_TIME_SCALE_S)
+	angle += alpha * (turn - angle)
+	
+	# Rotate the whole scene for the correct facing
 	rotation = angle
 	
-	velocity = 1000 * Vector2(sin(angle), 0)
-
+	# Physics
+	velocity = SPEED * Vector2(sin(angle), 0)
 	move_and_slide()
