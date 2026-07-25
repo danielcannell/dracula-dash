8b2c912cc2d77144e467f200e2f8addda1be9228
Jamie Wood <hypernerd1995@gmail.com>
2026-07-25 20:44:48 +0100

    Randomize hit pitch

--- scripts/child.gd
+++ scripts/child.gd
@@ -2,6 +2,8 @@ extends StaticBody2D
 
 func _ready():
 	add_to_group("children")
+	# randomize a bit
+	$Child_Hit.pitch_scale = randf_range(0.5, 2.0)
 
 func _physics_process(delta: float) -> void:
 	position.y += delta * Globals.cur_forward_speed
