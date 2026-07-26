a16a2705ece3a06067ed2c8f0d9046f45b00fe32
Jamie Wood <hypernerd1995@gmail.com>
2026-07-26 15:10:18 +0100

    Fix dead player marker movement

--- scripts/dead_player.gd
+++ scripts/dead_player.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	position.y += delta * Globals.cur_forward_speed
 	if position.y > 1000:
 		queue_free()
