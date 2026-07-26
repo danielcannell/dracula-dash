7b082e6818d91bce1284cc9f5906dae0f3bae063
Jamie Wood <hypernerd1995@gmail.com>
2026-07-26 17:48:33 +0100

    Add explosion sound

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -4,9 +4,10 @@ extends Node2D
 func _ready() -> void:
 	# Play your non-looping animation (e.g., "explosion")
 	$AnimatedSprite2D.play("explosion")
-	
+	$AudioStreamPlayer2D.play()
+
 	# Wait until the animation plays its last frame and finishes
 	await $AnimatedSprite2D.animation_finished
-	
+
 	# Remove/unload the node from memory and the scene tree
 	queue_free()
